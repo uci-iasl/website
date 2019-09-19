e16bd5cba797044d95ef3812d6bb4124256c3f27
Igor Burago <iburago@uci.edu>
2019-09-19 15:35:36 +1000

    Allow interactive filtering of bibliography entries
    Add a straightforward text-based filtering of bibliography entries.
    On every change of the contents of the filter text field, split the
    query into whitespace-separated words and hide all the entries that
    do not have occurrences of all of the words as substrings somewhere
    within them. The list of fields that are scanned for matches in each
    entry is found in the 'bib.entryFieldsSelector' array (see
    static/js/bibliography.js). Differences in case are ignored.
    
    Although the performance seems to be satisfactory for the foreseeable
    future, it might perhaps be justified to build a Trie-based index or a
    DAFSA-based matcher on page load.

--- static/js/bibliography.js
+++ static/js/bibliography.js
@@ -1,5 +1,74 @@
 "use strict";
+
 var bib = {
+	sectionSelector: "section.bibliography",
+	sectionGroupsSelector: ".group",
+	groupEntriesSelector: "ul.bibliography>li",
+	entryFieldsSelector: [
+		".authors", ".editors",
+		".title", ".journal", ".booktitle", ".series",
+		".organization", ".institution", ".school",
+		".publisher", ".address", ".date",
+		".note",
+	].join(","),
+	filterInputSelector: "input.filter",
+
+	initEntryTextFilter: function() {
+		var sectionElem = document.querySelector(bib.sectionSelector);
+		var groupElems = sectionElem.querySelectorAll(bib.sectionGroupsSelector);
+		var groups = [];
+		for (var groupElem of groupElems) {
+			var entryElems = groupElem.querySelectorAll(bib.groupEntriesSelector);
+			var entries = [];
+			for (var entryElem of entryElems) {
+				var fieldElems = entryElem.querySelectorAll(bib.entryFieldsSelector);
+				var values = [];
+				for (var fieldElem of fieldElems)
+					values.push(fieldElem.innerText.toLowerCase());
+				entries.push({
+					element: entryElem,
+					values: values,
+				});
+			}
+			groups.push({
+				element: groupElem,
+				entries: entries,
+			});
+		}
+		bib.entryGroups = groups;
+
+		bib.filterInputElem = sectionElem.querySelector(bib.filterInputSelector)
+		bib.filterInputElem.oninput = bib.filterEntries
+	},
+
+	doStringsMatch: function(strings, queryWords) {
+		for (var qw of queryWords) {
+			var found = false;
+			for (var str of strings) {
+				if (str.indexOf(qw) != -1) {
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+				return false;
+		}
+		return true;
+	},
+
+	filterEntries: function() {
+		var queryWords = bib.filterInputElem.value.toLowerCase().split(/\s+/);
+		for (var group of bib.entryGroups) {
+			var numVisible = group.entries.length
+			for (var item of group.entries) {
+				var matched = bib.doStringsMatch(item.values, queryWords);
+				item.element.style.display = (matched ? "list-item" : "none");
+				numVisible -= !matched;
+			}
+			group.element.style.display = (numVisible > 0 ? "block" : "none");
+		}
+	},
+
 	toggleBibtexDisplay: function(button) {
 		var entry = button;
 		while (entry && (!entry.classList.contains("bibentry") || !entry.id))
@@ -16,5 +85,7 @@ var bib = {
 			button.textContent = button.textContent.replace(/^▾/, "▸");
 			bibtex.style.display = "none";
 		}
-	}
+	},
 }
+
+window.onload = bib.initEntryTextFilter;
