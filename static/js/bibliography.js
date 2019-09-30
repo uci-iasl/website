ff97940e33ef7b4d1b6325b8557599619a817b28
Igor Burago <iburago@uci.edu>
2019-10-01 04:16:07 +1000

    Make 'et al.' clickable to show the full author or editor name list

--- static/js/bibliography.js
+++ static/js/bibliography.js
@@ -1,5 +1,15 @@
 "use strict";
+
 var bib = {
+	displayEtAlNames: function(button) {
+		var etalElem = button.parentNode;
+		if (!etalElem || !etalElem.classList.contains("etal")) return;
+		var etalContainer = etalElem.querySelector(".container");
+		if (!etalContainer) return;
+		etalContainer.style.display = "inline";
+		button.style.display = "none";
+	},
+
 	toggleBibtexDisplay: function(button) {
 		var entry = button;
 		while (entry && (!entry.classList.contains("bibentry") || !entry.id))
@@ -16,5 +26,5 @@ var bib = {
 			button.textContent = button.textContent.replace(/^▾/, "▸");
 			bibtex.style.display = "none";
 		}
-	}
+	},
 }
